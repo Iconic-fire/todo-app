1bf9bf3a2eea1b3c9e67237748d580275595d52f
Satyam Seth <satyam1998.1998@gmail.com>
2024-12-26 01:08:01 +0530

    Add todo model admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core.models import Todo
 
 # Register your models here.
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    """Todo Model Admin"""
+
+    list_filter = ('is_completed',)
+    search_fields = ('title', 'description')
+    list_display = (
+        'title',
+        'description',
+        'is_completed',
+        'due_date', 
+        'created_at', 
+        'updated_at',
+    )
