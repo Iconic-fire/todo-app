6b0950eb41ee6a5e408ac54cd3d05c26775ab3f9
Satyam Seth <satyam1998.1998@gmail.com>
2024-12-30 23:44:39 +0530

    Add todo detail url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,4 +6,5 @@ app_name = "core"
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name='home'),
     path('todos/', views.TodoListView.as_view(), name='todos'),
+    path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todos_detail'),
 ]
