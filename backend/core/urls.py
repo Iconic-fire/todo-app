5468eaefb2da51d5d1c56be1857750e8f68b01d5
Satyam Seth <satyam1998.1998@gmail.com>
2025-01-11 19:22:03 +0530

    Rename todo detail url pattern

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,6 +6,6 @@ app_name = "core"
 urlpatterns = [
     path('', views.TodoListView.as_view(), name='todo_list'),
     path('create/', views.TodoCreateView.as_view(), name='todo_create'),
-    path('<int:pk>/', views.TodoDetailView.as_view(), name='todos_detail'),
+    path('<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
     path('<int:pk>/update/', views.TodoUpdateView.as_view(), name='todo_update'),
 ]
