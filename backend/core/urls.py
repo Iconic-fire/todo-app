e72be3c4d590a5b23b601b9fc1093604e41404f2
Satyam Seth <satyam1998.1998@gmail.com>
2025-01-11 19:12:25 +0530

    Add todo update url pattern

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.TodoListView.as_view(), name='todos'),
     path('<int:pk>/', views.TodoDetailView.as_view(), name='todos_detail'),
     path('create/', views.TodoCreateView.as_view(), name='todos_create'),
+    path('<int:pk>/update/', views.TodoUpdateView.as_view(), name='todo_update'),
 ]
