07435946dd1378c7fe2c5aaba5c9ad7317b5294b
Satyam Seth <satyam1998.1998@gmail.com>
2025-01-11 20:45:41 +0530

    Add todo update completed view

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, View
 
 from core.models import Todo
-from core.forms import TodoCreateForm, TodoUpdateForm
+from core.forms import TodoCreateForm, TodoUpdateCompletedForm, TodoUpdateForm
 
 # Create your views here.
 
@@ -40,3 +42,21 @@ class TodoUpdateView(UpdateView):
     form_class = TodoUpdateForm
     template_name_suffix= '_update'
     success_url = reverse_lazy("core:todo_list")
+
+
+class TodoUpdateCompletedView(View):
+    """Todo Update Completed"""
+
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
+        """Method to handle todo completed post request"""
+
+        todo = get_object_or_404(Todo, pk=pk)
+        form = TodoUpdateCompletedForm(request.POST)
+
+        if form.is_valid():
+            todo.is_completed = form.cleaned_data['completed']
+            todo.save()
+
+        # TODO: Add message
+
+        return redirect('core:todo_list')
