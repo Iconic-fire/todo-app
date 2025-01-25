4a7205b18bb187f4b10a07f8ec604eacc078999f
Satyam Seth <satyam1998.1998@gmail.com>
2025-01-25 09:12:56 +0530

    Add no record found component

---
+++ frontend/src/components/Empty.tsx
@@ -0,0 +1,9 @@
+function NoRecordFound() {
+  return (
+    <p className="text-center text-lg text-gray-500 dark:text-gray-300">
+      No record found
+    </p>
+  );
+}
+
+export default NoRecordFound;
