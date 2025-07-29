11e316231fe0f6e7fc6f0b08132377745a30040c
Satyam Seth <satyam1998.1998@gmail.com>
2025-07-30 00:34:25 +0530

    Add accounts api urls into project urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,8 @@ from core.routers import todo_router
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(todo_router.urls)),
+    # TODO: Fix BrowsableAPIRenderer not including this endpoint /api/ (broken)
+    path("api/accounts/", include("accounts.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
