8c37611c3d2b09ec4f585b78e1a1c62e05d8699e
Satyam Seth <satyam1998.1998@gmail.com>
2025-07-29 23:55:08 +0530

    Add login api view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.generics import GenericAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from accounts.serializers import LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
+
+
+class LoginView(GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        tokens = get_tokens_for_user(user)
+        # TODO: store refresh token to client cookie http only
+        return Response({"message": "Login successful", "tokens": tokens})
