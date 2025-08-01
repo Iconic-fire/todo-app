2f1847d695cb6cd94841a761ecffde43c629ffc0
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-02 02:10:52 +0530

    Add untility function to redirect user to login page for not component code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router";
 import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import { setNavigate } from "./auth/redirects";
 
 function App() {
-   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        } />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setNavigate(navigate);
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
