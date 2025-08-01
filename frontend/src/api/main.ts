ca2c5b9ad923a6151ae3f66f7492327e39b26f6b
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-02 02:12:58 +0530

    Use axios instance for user apis

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -1,7 +1,7 @@
+import {API_BASE_URL, axiosInstance} from "./axios";
 import { Configuration, AccountsApi, TodosApi } from "./client";
 
-// TODO: base url must be come from enticement variable
-const config = new Configuration({ basePath: "http://127.0.0.1:8000" })
+const config = new Configuration({ basePath: API_BASE_URL });
 
-export const accountsApi = new AccountsApi(config)
-export const todoApi = new TodosApi(config);
+export const accountsApi = new AccountsApi(config);
+export const todoApi = new TodosApi(config,  undefined, axiosInstance);
