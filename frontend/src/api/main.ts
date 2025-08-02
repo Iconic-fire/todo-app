6ac01dea570d3e9756677fbcd27a0e1df8c0687a
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-02 21:59:15 +0530

    Add Header component with logout button

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -4,4 +4,5 @@ import { Configuration, AccountsApi, TodosApi } from "./client";
 const config = new Configuration({ basePath: API_BASE_URL });
 
 export const accountsApi = new AccountsApi(config);
+export const accountsApiAuthenticated = new AccountsApi(config, undefined, axiosInstance);
 export const todoApi = new TodosApi(config,  undefined, axiosInstance);
