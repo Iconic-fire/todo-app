800f6a87d18669ba3e86ac8c4de0b323e724759c
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-03 20:29:27 +0530

    Add signup api url

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from accounts import views
 
 urlpatterns = [
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
