23b47b48a83131c7b6d84aa8c6e8aafc7e6dad1a
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-05 01:45:34 +0530

    Add password reset confirm url

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
     path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('reset-password-validate/', views.PasswordResetValidateView.as_view(), name='reset_password_validate'),
+    path('password-reset-confirm/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
