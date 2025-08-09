8af91ada690de82ef1f6acd50e3bc2aeb99215cc
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-10 01:26:32 +0530

    Add api clients module

---
+++ frontend/src/api/apiClients.ts
@@ -0,0 +1,8 @@
+import { Configuration, AccountsApi, TodosApi } from "./client";
+import { axiosInstance, plainAxios } from "./axiosConfig";
+
+const config = new Configuration();
+
+export const unauthenticatedAccountsApi = new AccountsApi(config, undefined, plainAxios);
+export const accountsApi = new AccountsApi(config, undefined, axiosInstance);
+export const todoApi = new TodosApi(config, undefined, axiosInstance);
