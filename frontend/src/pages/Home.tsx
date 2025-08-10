b92b7bf525191fe2679c7010e4ff9f065455ea68
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-10 19:13:37 +0530

    Add retry button if unable to fetch todos

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -71,8 +71,24 @@ export function Home() {
   }
 
   if (failed === true) {
-    // TODO: add retry button
-    return <h1>Unable to fetch</h1>;
+    return <div className="h-svh flex flex-col items-center justify-center bg-stone-800">
+      <h1 className="text-white text-lg mb-4">Unable to fetch your todos.</h1>
+      <button
+        onClick={() => {
+          setLoading(true);
+          setFailed(false);
+          // Retry fetching data
+          todoApi
+            .todosList()
+            .then((res) => setTodos(res.data.results))
+            .catch(() => setFailed(true))
+            .finally(() => setLoading(false));
+        }}
+        className="text-white bg-red-500 hover:bg-red-600 px-4 py-2 rounded"
+      >
+        Retry
+      </button>
+    </div>;
   }
 
   return (
