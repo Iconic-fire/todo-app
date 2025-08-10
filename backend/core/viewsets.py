82e4cc711493dd50da855549bf8840ae894239b9
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-10 22:37:58 +0530

    Apply current user context for todo viewset

--- backend/core/viewsets.py
+++ backend/core/viewsets.py
@@ -1,10 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Todo
+from core.permissions import IsOwner
 from core.serializers import TodoSerializer
 
 class TodoViewSet(ModelViewSet):
     """Todo View Set"""
 
-    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes=[IsOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
