021dbacca8030b3219b75f14ac06bd4299f3ff96
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-10 22:36:51 +0530

    Make user field read only in todo serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -5,6 +5,8 @@ from core.models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     """Todo Serializer"""
 
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Todo
-        fields= '__all__'
+        fields = '__all__'
