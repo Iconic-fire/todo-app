51e915c6e4c62abc2ca2c3985345415a40383542
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-20 23:30:24 +0530

    Add accounts url in project url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("accounts/", include("accounts.urls")),
     path("todos/", include("core.urls")),
     path("", views.HomeTemplateView.as_view(), name='home'),
 ]
